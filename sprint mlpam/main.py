485d1a5a175d0f8e30d6c42f3a51702d69a3bc6d
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-04 10:59:47 -0300

    corrigindo

--- sprint mlpam/main.py
+++ sprint mlpam/main.py
@@ -1,4 +1,4 @@
-# PREPARANDO O AMBIENTE
+# preparando o ambiente
 from collections import Counter
 import pandas as pd
 
@@ -6,29 +6,35 @@ pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 
-# CARREGANDO OS DADOS
+# criando os dataframes
 caminho_arquivo = 'dados.xlsx'
 df_original = pd.read_excel(caminho_arquivo)
+# dataframe copia do original
 df = df_original.copy()
 
+# total de dados
 print("Total original:", len(df_original))
 
-# TRATAMENTO 
+# selecionando as colunas, criando as tabelas e transformando em numeros
 df['vezes_semana'] = pd.to_numeric(df['vezes_semana'], errors='coerce')
 df['energia_kwh'] = pd.to_numeric(df['energia_kwh'], errors='coerce')
 
-# Remover apenas linhas com erro nas colunas usadas
+# remover apenas linhas com erro nas colunas usadas
 df = df.dropna(subset=['vezes_semana', 'energia_kwh'])
 
-# VISUALIZAÇÃO INICIAL
-print("DADOS")
+# mostrando os primeiros dados da tabela
+print("dados")
 print(df.head())
 
-# VARIÁVEL QUANTITATIVA DISCRETA
-print("TABELA DISCRETA")
+print()
 
+# variavel quantitativa discreta
+print("tabela discreta")
+
+# selecionando a coluna vezes_semana do dataframe df
 dados_discretos = df['vezes_semana']
 
+# criando as variaveis das frequencias
 fi = pd.Series(Counter(dados_discretos)).sort_index()
 fia = fi.cumsum()
 fr = 100 * fi / fi.sum()
@@ -52,18 +58,23 @@ tabela_discreta = pd.concat([tabela_discreta, total.to_frame().T])
 
 print(tabela_discreta)
 
+print()
+
 # Insights
 # Usuários com baixa frequência de recarga são minoria
 # Frequências intermediárias baixas também têm menor participação
 
-# VARIÁVEL QUANTITATIVA CONTÍNUA
+# variavel quantitativa continua
 print("TABELA CONTÍNUA")
 
+# selecionando a coluna energia_kwh do dataframe df
 dados_continuos = df['energia_kwh']
 
+# numero de classes
 num_classes = 5
 classes = pd.cut(dados_continuos, bins=num_classes, right=False)
 
+# criando as variaveis das frequencias
 fi = classes.value_counts().sort_index()
 fia = fi.cumsum()
 fr = (100 * fi / fi.sum()).round(2)
